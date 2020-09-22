8f82f9d278a56bcea1a214b231f44cf4aae6c2ed
Baran Emre Türkmen <43263983+baranemreturkmen@users.noreply.github.com>
2020-09-22 14:33:30 +0300

    Add files via upload

--- deneme.py
+++ deneme.py
@@ -2,11 +2,12 @@ import sayı_oku                                                                
 
 sayı_oku =sayı_oku.Obje_Oluştur()                                                                                       #Obje oluşturuldu
 
-sayı_oku.oku(sayı_gir=34283676556)                                                                                      #Oluşturulan objenin oku metoduna okunması istenen sayı girildi
+sayı_oku.oku(sayı_gir=8324793423423423423423432482398472389427938472889598572885298835728975828359283579293578923758273583925782937582935798275982737598237582735927)
+#Oluşturulan objenin oku metoduna okunması istenen sayı girildi
 
 say_deger,okunus,bas_say = sayı_oku.kaydet(kayıt=True)                                                                  #Okunan sayının değerleri değişkene atıldı .
 
-print(say_deger,okunus,bas_say)                                                                                         #Atanan değerlere konsolda bakıyorum . 
+print(say_deger,okunus,bas_say)                                                                                         #Atanan değerlere konsolda bakıyorum .
 
 print(say_deger)
 
